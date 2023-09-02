7154ab987162c29b06ddd0e5274a231ebad0e942
Taky0nDon <126304313+Taky0nDon@users.noreply.github.com>
2023-09-02 08:09:18 -0400

    switching from CSV to SQLite database

--- Day 62 Advanced flask CSV readme.md
+++ Day 62 Advanced flask CSV readme.md
@@ -1,7 +1,13 @@
 #flask
 
+for details on how to use flask-sqlalchemy see: [[day63readme]]
+
 ## Coffee & Wifi
 
 ### The `<select>` element
 
-* You don't need to add form tags if you use the render_form() method from bootstrap-flask:c
+* You don't need to add form tags if you use the render_form() method from bootstrap-flask
+* instead of returning my cafe() function, which doesn't actually change the URL, i could have used the redirect() function
+from Flask: `return redirect(url_for('cafes'))`
+
+now this should appear in obsidian
--- main.py
+++ main.py
@@ -1,31 +1,18 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap5
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, SelectField
-from wtforms.validators import DataRequired, URL
-import csv
+from wtforms import StringField, SubmitField, SelectField, IntegerField
+from wtforms.validators import DataRequired, URL, number_range
+from html import escape
+from flask_sqlalchemy import SQLAlchemy
 
-'''
-Red underline? Install the required packages first: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from requirements.txt for this project.
-'''
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 bootstrap = Bootstrap5(app)
-
-
-def add_line_to_csv(path_to_csv: str, new_line: list[str]) -> None:
-    with open(path_to_csv, "a", encoding="utf-8") as file:
-        new_line_stringified = ",".join(new_line)
-        file.write("\n" + new_line_stringified)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///cafe-db.db"
+db = SQLAlchemy()
+db.init_app(app)
 
 
 class CafeForm(FlaskForm):
@@ -35,12 +22,35 @@ class CafeForm(FlaskForm):
     close_4 = StringField("close", validators=[DataRequired()])
     coffee_5 = SelectField("coffee", validators=[DataRequired()], choices=["☕️"*i if i > 0 else "✘" for i in range(0,6)])
     wifi_6 = SelectField("wifi", validators=[DataRequired()], choices=["💪"*i if i > 0 else "✘" for i in range(0,6)])
- 
+    power_7 = SelectField("power", validators=[DataRequired()], choices=["🔌"*i if i > 0 else "✘" for i in range(0,6)])
+    submit = SubmitField('Submit')
+class Cafes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    cafe_name = db.Column(db.String())
+    location_url = db.Column(db.String())
+    open = db.Column(db.String())
+    close = db.Column(db.String())
+    coffee = db.Column(db.String())
+    choices = db.Column(db.String())
+    wifi = db.Column(db.String())
+    power = db.Column(db.String())
+
+
+def stringify_data(data: list[str]) -> str:
+    stringified = ",".join(data)
+    return stringified
+
+
+def add_line_to_csv(path_to_csv: str, new_line: str) -> None:
+    with open(path_to_csv, "a", encoding="utf-8") as file:
+        file.write(f'"\n"{new_line}')
+
+
+
 
 # Exercise:
 # add: Location URL, open time, closing time, coffee rating, wifi rating, power outlet rating fields
-      power_7 = SelectField("power", validators=[DataRequired()], choices=["🔌"*i if i > 0 else "✘" for i in range(0,6)])
-    submit = SubmitField('Submit')
+
 # make coffee/wifi/power a select element with choice of 0 to 5.
 #e.g. You could use emojis ☕️️️/💪/✘/🔌
 # make all fields required except submit
@@ -49,8 +59,8 @@ class CafeForm(FlaskForm):
 
 
 # all Flask routes below
-
-def home):
+@app.route("/")
+def home():
     return render_template("index.html")
 
 
@@ -58,14 +68,18 @@ def home):
 @app.route('/add', methods=["GET", "POST"])
 def add_cafe():
     form = CafeForm()
-    if form.validate_on_submit(): added_cafe = [form.cafe_name1.data,
-                      form.location_url_2.data,
-                      form.open_3.data,
-                      form.close_4.data,
-                      form.coffee_5.data,
-                      form.wifi_6.data,
-                      form.power_7.data]
-        add_line_to_csv("cafe-data.csv", added_cafe)
+    if form.validate_on_submit():
+        added_cafe = [fr"{form.cafe_name1.data}",
+                      fr"{form.location_url_2.data}",
+                      fr"{form.open_3.data}",
+                      fr"{form.close_4.data}",
+                      fr"{form.coffee_5.data}",
+                      fr"{form.wifi_6.data}",
+                      fr"{form.power_7.data}"]
+        [print(e) for e in added_cafe]
+        new_line = stringify_data([escape(e) for e in added_cafe])
+        print(new_line)
+        add_line_to_csv("cafe-data.csv", new_line)
         return redirect(url_for('cafes'))
     # Exercise:
     # Make the form write a new row into cafe-data.csv
@@ -76,10 +90,39 @@ def add_cafe():
 
 @app.route('/cafes')
 def cafes():
+    print(DelForm.get_csv_length("cafe-data.csv"))
     with open('cafe-data.csv', newline='', encoding='utf-8') as csv_file:
         csv_data = csv.reader(csv_file, delimiter=',')
         list_of_rows = [row for row in csv_data]
+    for row in list_of_rows:
+        print(row, f"{len(row)=}")
     return render_template('cafes.html', cafes=list_of_rows)
+
+
+@app.route("/del/", methods=["GET", "POST"])
+def show_delete_form():
+    delete_form = DelForm()
+    print(delete_form.max)
+    if delete_form.validate_on_submit():
+        print(type(delete_form.row_to_delete.data))
+        print(delete_form.row_to_delete.data)
+        return redirect(url_for('delete_row', line_to_delete=delete_form.row_to_delete.data))
+    return render_template("delete-row.html", form=delete_form)
+@app.route("/del/<int:line_to_delete>", methods=["GET", "POST"])
+def delete_row(line_to_delete: int) -> str:
+    if line_to_delete < 1:
+        return "You cannot delete the title row!"
+    with open("cafe-data.csv", encoding="utf-8") as csv_file:
+        lines = csv_file.readlines()
+        new_data = "".join([line for i, line in enumerate(lines) if i != line_to_delete])
+        print(f"{new_data=}")
+    keep_going = input("Continue with delete operation?")
+    if keep_going == "y":
+        with open("cafe-data.csv", "w", encoding="utf-8") as csv_file:
+            csv_file.write(new_data)
+            return f"{line_to_delete} deleted."
+    else:
+        return "Turning back was a wise choice."
 
 
 if __name__ == '__main__':
